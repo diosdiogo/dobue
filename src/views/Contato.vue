714add52a5c5c0d97fad12b202b14f53e42a600e
@diogoCesar <diogo.andrade@plss.com.br>
2021-05-03 08:08:52 -0300

    pag cliente

---
+++ src/views/Contato.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <Toolbar style="margin-bottom:15px;"/>
+            <v-main style="background-color:#F3F3F3">
+                <v-container class="pb-5">
+                    <b-row class="text-center">
+                        <b-col><h1 class="font-header">contato</h1></b-col>
+                    </b-row>
+                </v-container>
+            </v-main>
+    </div>
+</template>
